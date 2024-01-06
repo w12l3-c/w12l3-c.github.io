0103613d814ee02ad2c6c02c86f9c4715f168e9c
w12l3-c <wallace.lee0412@gmail.com>
2024-01-06 17:22:12 -0500

    Attempt to fix

--- person_website/src/index.js
+++ person_website/src/index.js
@@ -8,9 +8,10 @@ import { BrowserRouter as Router, Route, Link, Routes, BrowserRouter, HashRouter
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    {/* <HashRouter>
       <App />
-    </HashRouter>
+    </HashRouter> */}
+    <App />
   </React.StrictMode>
 );
 
