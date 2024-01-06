89a54196d4fa9f055f34dd0ff3406b7105913a9f
w12l3-c <wallace.lee0412@gmail.com>
2024-01-06 18:32:15 -0500

    Attempt fix

--- person_website/src/App.js
+++ person_website/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from "react-dom";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './Home';
 import Experience from './Experience';
